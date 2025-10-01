c26d3d4bddb0240d94e368d6c45bfd937917393d
Johnmk916 <jnmk0032100@gmail.com>
2025-10-01 21:06:53 +0800

    update

--- src/lib/resend.ts
+++ src/lib/resend.ts
@@ -2,7 +2,8 @@
 
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+export const resend = new Resend(process.env.RESEND_API_KEY);
+
 
 export async function sendEmail(formData: {
   firstName: string;
